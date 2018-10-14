189bd654d67b67a73cbcfcb2493ecca2753c2fb7
Alex Kofke <akofke@gmail.com>
2018-10-14 15:44:45 -0400

    Add model for users
    Task #22

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = get_user_model()
+
+
+admin.site.register(get_user_model(), CustomUserAdmin)
