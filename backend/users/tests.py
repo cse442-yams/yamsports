6795b5024145860b14effeb362f434eba8eeab5d
Alex Kofke <akofke@gmail.com>
2018-10-19 23:04:07 -0400

    Add basic tests for user authentication

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+import json
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
 
-# Create your tests here.
+from .models import CustomUser
+
+
+class UserAuthTest(APITestCase):
+    client = APIClient()
+
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(username="test", email="test@gmail.com", password="testing")
+
+    def test_valid_login(self):
+        url = reverse("rest_login")
+
+        response = self.client.post(
+            url,
+            data=json.dumps({"username": "test", "password": "testing"}),
+            content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("key", response.data)
+
+    def test_invalid_login(self):
+        url = reverse("rest_login")
+
+        response = self.client.post(
+            url,
+            data=json.dumps({"username": "test", "password": "testing wrong"}),
+            content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn("key", response.data)
+
