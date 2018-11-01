f63fa92a4341af6e831a2c2ca7859e9afa0790cc
Alex Kofke <akofke@gmail.com>
2018-11-01 14:26:01 -0400

    Add detail view for players

--- backend/nba_profile/urls.py
+++ backend/nba_profile/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from nba_profile import views
 
 urlpatterns = [
-    path('players/', views.NBAPlayersList.as_view())
+    path('players/', views.NBAPlayersList.as_view()),
+    path('players/<int:pk>/', views.NBAPlayerDetail.as_view())
 ]
--- backend/nba_profile/views.py
+++ backend/nba_profile/views.py
@@ -1,9 +1,14 @@
 from rest_framework import generics
 
 from nba_profile.models import NBAPlayer
-from nba_profile.serializers import NBAPlayerBasicSerializer
+from nba_profile.serializers import NBAPlayerBasicSerializer, NBAPlayerDetailSerializer
 
 
 class NBAPlayersList(generics.ListAPIView):
     queryset = NBAPlayer.objects.all()
     serializer_class = NBAPlayerBasicSerializer
+
+
+class NBAPlayerDetail(generics.RetrieveAPIView):
+    queryset = NBAPlayer.objects.all()
+    serializer_class = NBAPlayerDetailSerializer
