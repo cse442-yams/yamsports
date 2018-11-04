22f7dcb1a2c9ee5d8e2952e33ba446bbf4051e4a
Alex Kofke <akofke@gmail.com>
2018-11-04 15:07:50 -0500

    Create Feed endpoint for user team custom news
    Queries news items related to players on given user team and returns a
    filtered rss feed

--- backend/nba_profile/urls.py
+++ backend/nba_profile/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 from nba_profile import views
 
@@ -6,5 +6,6 @@ urlpatterns = [
     path('players/', views.NBAPlayersList.as_view(), name='nba-players-list'),
     path('players/<int:pk>/', views.NBAPlayerDetail.as_view(), name='nba-player-detail'),
     path('teams/', views.UserTeamsList.as_view(), name='user-teams-list'),
-    path('teams/<int:pk>', views.UserTeamUpdate.as_view(), name='user-teams-update')
+    path('teams/<int:pk>', views.UserTeamUpdate.as_view(), name='user-teams-update'),
+    path('', include('newsfeed.urls'))
 ]
