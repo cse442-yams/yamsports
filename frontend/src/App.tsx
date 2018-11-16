2222263a234a6c8745f9522a8b2352b46c3d0184
Alex Kofke <akofke@gmail.com>
2018-11-15 23:43:22 -0500

    Try a different purple as primary color

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,18 @@
 import green from "@material-ui/core/colors/green";
-import purple from "@material-ui/core/colors/purple";
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import {createMuiTheme, MuiThemeProvider} from "@material-ui/core/styles";
 import * as React from 'react';
-import { CssBaseline } from "@material-ui/core";
+import {CssBaseline} from "@material-ui/core";
 import LandingPage from "./LandingPage";
 import ProfilePage from "./ProfilePage";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import {authStore} from "./auth/AuthStore";
 import DevTools from "mobx-react-devtools";
 import {observer} from "mobx-react";
+import {deepPurple} from "@material-ui/core/colors";
 
 const theme = createMuiTheme({
   palette: {
-    primary: purple,
+    primary: deepPurple,
     secondary: green
   }
 });
