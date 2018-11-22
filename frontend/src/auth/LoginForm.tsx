54a990c388b3921e4e1cab667f10f6ab555b5b8c
Alex Kofke <akofke@gmail.com>
2018-11-21 23:15:51 -0500

    Fix login and register forms to submit with enter key

--- frontend/src/auth/LoginForm.tsx
+++ frontend/src/auth/LoginForm.tsx
@@ -10,7 +10,7 @@ import {
     WithStyles
 } from "@material-ui/core";
 import {authStore} from "./AuthStore";
-import {ChangeEvent} from "react";
+import {ChangeEvent, FormEvent} from "react";
 import {observer} from "mobx-react";
 
 
@@ -25,7 +25,10 @@ class LoginForm extends React.Component<WithStyles<typeof styles>, any> {
 
     private handleUsernameChange = (e: ChangeEvent<HTMLTextAreaElement>) => authStore.setUsername(e.target.value);
     private handlePasswordChange = (e: ChangeEvent<HTMLTextAreaElement>) => authStore.setPassword(e.target.value);
-    private handleSubmit = () => authStore.login();
+    private handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        authStore.login();
+    };
 
     public componentWillUnmount() {
         authStore.reset()
@@ -35,16 +38,18 @@ class LoginForm extends React.Component<WithStyles<typeof styles>, any> {
         return (
             <React.Fragment>
                 <DialogTitle>Login</DialogTitle>
-                <DialogContent>
-                    {authStore.nonfieldError !== '' && <Typography color={"error"}>{authStore.nonfieldError}</Typography>}
-                    <TextField autoFocus={true} label="Username" fullWidth={true} margin="normal"
-                               onChange={this.handleUsernameChange} error={authStore.errorValues.username !== ''} helperText={authStore.errorValues.username}/>
-                    <TextField label="Password" type="password" fullWidth={true} margin="normal"
-                               onChange={this.handlePasswordChange} error={authStore.errorValues.password !== ''} helperText={authStore.errorValues.password}/>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={this.handleSubmit}>Log In</Button>
-                </DialogActions>
+                <form onSubmit={this.handleSubmit}>
+                    <DialogContent>
+                        {authStore.nonfieldError !== '' && <Typography color={"error"}>{authStore.nonfieldError}</Typography>}
+                        <TextField autoFocus={true} label="Username" fullWidth={true} margin="normal"
+                                   onChange={this.handleUsernameChange} error={authStore.errorValues.username !== ''} helperText={authStore.errorValues.username}/>
+                        <TextField label="Password" type="password" fullWidth={true} margin="normal"
+                                   onChange={this.handlePasswordChange} error={authStore.errorValues.password !== ''} helperText={authStore.errorValues.password}/>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button type={"submit"}>Log In</Button>
+                    </DialogActions>
+                </form>
                 {/*<Divider className={this.props.classes.dialogDivider}/>*/}
                 {/*<DialogContent>*/}
                 {/*/!*<Button fullWidth>Continue without an account</Button>*!/*/}
