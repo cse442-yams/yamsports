0d4aacd7c5e0050ae5bb249d9cc56076eca9007b
Alex Kofke <akofke@gmail.com>
2018-11-29 21:12:12 -0500

    More heroku settings

--- backend/yamsports/settings.py
+++ backend/yamsports/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '84ehh#+7k8_wq_t9ya5vf4x94e6#eq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yamsports.surge.sh', 'yamsports.heroku.com', '127.0.0.1']
 
 
 # Application definition
